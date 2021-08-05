ed0629f50ee36299e879a40443c78cf5aa8d9925
mkato1013 <mkatochoco1013@gmail.com>
2021-08-05 21:08:34 +0900

    修正（レビュー後）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation {password}
     nickname              {Faker::Name.initials(number: 2)}
-    last_name             {Faker::Name.initials(number: 2)}
-    first_name            {Faker::Name.initials(number: 2)}
-    last_name_kana        {Faker::Name.initials(number: 2)}
-    first_name_kana       {Faker::Name.initials(number: 2)}
+    last_name             {'鈴木'}
+    first_name            {'太郎'}
+    last_name_kana        {'スズキ'}
+    first_name_kana       {'タロウ'}
     birthday              {Faker::Date.birthday}
-    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe User, type: :model do
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
+      binding.pry
       expect(another_user.errors.full_messages).to include "Email has already been taken"
     end
 
@@ -46,5 +47,46 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
 
+    it 'passwordは半角英語のみでは登録できない' do
+      @user.password = "aaaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+    it 'passwordは半角数字のみでは登録できない' do
+      @user.password = "000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+    it 'passwordは全角英数混合では登録できない' do
+      @user.password = "A２４G６７"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+    it 'last_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+      @user.last_name = "000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name is invalid. Input full-width characters."
+    end
+
+    it 'first_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+      @user.first_name = "000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name is invalid. Input full-width characters."
+    end
+
+    it 'last_name_kanaは全角カタカナ以外では登録できないこと' do
+      @user.last_name_kana = "000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name kana is invalid. Input full-width katakana characters."
+    end
+
+    it 'first_name_kanaは全角カタカナ以外では登録できないこと' do
+      @user.first_name_kana = "000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana is invalid. Input full-width katakana characters."
+    end
   end
 end
