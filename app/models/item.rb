58b6d74eafacbe23c98e4d8831ecaddff54ed8f1
mkato1013 <mkatochoco1013@gmail.com>
2021-08-09 09:44:49 +0900

    テスト完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,14 +13,14 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :introduction
-    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    with_options numericality: { other_than: 0 , message: "can't be blank"} do
       validates :category_id
       validates :status_id
       validates :claim_id
       validates :area_id
       validates :send_day_id
     end
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 3, less_than_or_equal_to: 9999999, format: {with: /\A[0-9]\z/} ,message: "is invalid"}
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, format: {with: /\A[0-9]\z/} ,message: "is invalid"}
 
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    # image                   {'star.png'}
+    item_name               {'商品の名前'}
+    introduction            {'商品の説明です。'}
+    category_id             {2}
+    status_id               {2}
+    claim_id                {2}
+    area_id                 {2}
+    send_day_id             {2}
+    price                   {'300'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/star.png'), filename: 'star.png')
+    end
   end
 end
